36618a43441556d98756b99babd1fe3f794c14b1
ANANT <anantking11@gmail.com>
2024-04-07 20:59:01 +0530

    first commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 
 const MONGODB_URI=process.env.MONGODB_URI;
+
 let cached = (global as any).mongoose || {conn:null ,promise:null};
 
 export const connectToDatabase=async()=>{
@@ -15,7 +16,7 @@ export const connectToDatabase=async()=>{
         dbName:'evently',
         bufferCommands:false,
     })
-
+    console.log("Database connected")
     cached.conn=await cached.promise;
     return cached.conn;
 }
